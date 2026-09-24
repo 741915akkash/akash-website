432e586a0de8c87a04217d6d2706286fc6d0fc3d
Akash Saini <cheetah74191915@gmail.com>
2026-09-24 12:22:48 +0530

    catch-all page added

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,14 @@
+<script setup>
+const route = useRoute();
+
+if (route.fullPath.includes('akash-website-three.vercel.app')) {
+  await navigateTo('/', {
+    redirectCode: 301,
+  });
+} else {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page not found',
+  });
+}
+</script>
